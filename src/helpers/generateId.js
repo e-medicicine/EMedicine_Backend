c4e58a52d45fa1f07217fd48431f099819281f45
rubenevivancos <rubenevivancos@hotmail.com>
2024-08-02 01:26:36 -0500

    xxx

--- src/helpers/generateId.js
+++
@@ -1,9 +0,0 @@
-/**
- * This function returns a random ID for validating an email user
- */
-
-const generateID = () => {
-  return Date.now().toString(32) + Math.random().toString(32).substring(2)
-}
-
-export default generateID
--- src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import authController from "../controllers/auth.controller.js";
-
-const router = Router();
-router.post("/", authController.login);
-router.post("/logout", authController.logout);
-router.get("/confirm/:confirmationString", authController.confirm);
-
-export default router;
