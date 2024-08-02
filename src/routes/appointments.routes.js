d7dba4f5fbb618dda21e95637bda983a5550f0dc
rubenevivancos <rubenevivancos@hotmail.com>
2024-08-02 09:46:08 -0500

    ss

--- src/routes/appointments.routes.js
+++ src/routes/appointments.routes.js
@@ -9,7 +9,8 @@ router.get("/getAllAppo/:id/:state?", controllerAppo.getAppoById);
 
 router.get("/getAppoDoc/:id/:date?", controllerAppo.getAppoDoc);
 
-router.get("/getFreeSlotDoc/:id/:date", controllerAppo.getFreeSlotDoc);
+//router.get("/getFreeSlotDoc/:id/:date", controllerAppo.getFreeSlotDoc);
+router.get("/getFreeSlotDoc/:id", controllerAppo.getFreeSlotDoc);
 //router.get("/getFreeSlotDoc", controllerAppo.getFreeSlotDoc);
 
 export default router;
